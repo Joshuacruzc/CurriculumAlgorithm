eabebf20d92649e6302ef45606370cfebb702a6a
javierbustillo <javier.bustillo@upr.edu>
2019-04-06 14:27:30 -0400

    Added StudentPlan

---
+++ StudentPlan.py
@@ -0,0 +1,10 @@
+
+class StudentPlan:
+
+    def __init__(self, curriculum, max_credit):
+        self.curriculum = curriculum
+        self.max_credit = max_credit
+
+    def build_plan(self):
+        pass
+        
