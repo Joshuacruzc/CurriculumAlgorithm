6cc1f33f63108f7234c0fc4c77bbc0aea7a47bb6
Joshua Cruz <joshua.cruz15@upr.edu>
2019-06-26 19:10:54 -0400

    allow CORS

--- CurriculumAlgorithmWebApp/settings.py
+++ CurriculumAlgorithmWebApp/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'CurriculumAlgorithmWebApp.urls'
 
