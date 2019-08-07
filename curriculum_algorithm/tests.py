be7e1ab6092dc1b3615af545348d79758ab4320f
Joshua Cruz <joshua.cruz15@upr.edu>
2019-08-07 00:26:41 -0400

    Added CurrentUser view, User serializer and tests for Authentication

--- curriculum_algorithm/tests.py
+++ curriculum_algorithm/tests.py
@@ -8,10 +8,8 @@ from curriculum_algorithm.models import StudentPlan
 from import_curriculum import import_curriculum
 
 
-class StudentPlanTestCase(APITestCase):
-    def setUp(self):
-        user = get_user_model().objects.create(username='test_user', password='test')
-        self.client.force_authenticate(user=user)
+class CARequestsMixin:
+    client = None
 
     def post_student_plan(self, curriculum_id, max_credits):
         url = reverse('create_student_plan')
@@ -25,6 +23,12 @@ class StudentPlanTestCase(APITestCase):
     def accommodate_remaining_courses(self, student_plan_id):
         url = reverse('build_plan', args=(student_plan_id,))
         return self.client.get(url, format='json')
+
+
+class StudentPlanTestCase(CARequestsMixin, APITestCase):
+    def setUp(self):
+        user = get_user_model().objects.create(username='test_user', password='test')
+        self.client.force_authenticate(user=user)
 
     def test_create_student_plan(self):
         """
@@ -69,3 +73,25 @@ class StudentPlanTestCase(APITestCase):
         response = self.accommodate_remaining_courses(student_plan_id=created_student_plan['id'])
         retrieved_student_plan = json.loads(response.content)
         self.assertEqual(0, len(retrieved_student_plan['remaining_courses']), 'Courses left without accommodating.')
+
+
+class AuthenticationTestCase(CARequestsMixin, APITestCase):
+
+    def test_get_user_info(self):
+        """
+        We can retrieve User info using API
+        """
+        url = reverse('get_current_user')
+        user = get_user_model().objects.create(username='test_user', password='test')
+
+        self.client.force_authenticate(user=user)
+        response = self.client.get(url, format='json')
+        data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK, 'Server did not return Status Code: 200')
+        self.assertEqual(data['username'], user.username)
+
+    def test_unauthorized_requests(self):
+        # TODO: Test all endpoints that require authentication
+        response = self.post_student_plan(curriculum_id=1, max_credits=16)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
