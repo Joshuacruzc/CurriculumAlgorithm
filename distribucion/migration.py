28a4078b7001b7be782509c3ff3c5feabb16b99e
Jav <javier.bustillo@upr.edu>
2020-07-19 12:49:47 -0400

    Average Average

--- distribucion/migration.py
+++ distribucion/migration.py
@@ -4,25 +4,34 @@ from pyspark.sql.dataframe import DataFrame
 
 spark = SparkSession.builder.appName("Test").getOrCreate()
 
-sheets = ['VER-1', 'SEM-1', 'SEM-2']
-file_names = ['notas_2019-2020.xlsx']
+sheets = ['SEM-1', 'SEM-2']
+file_names = ['notas_2019-2020.xlsx', 'notas_2017-2018-2.xlsx', 'notas_2016-2017-2.xls']
 
 data_frames = []
 
+count = 0
 for file_name in file_names:
     for sheet in sheets:
+        print(file_name)
         pandas_frame = pandas.read_excel(file_name, sheet_name=sheet, inferSchema='true')
         data_frame = spark.createDataFrame(pandas_frame)
         data_frame = data_frame.replace(float('nan'), 0)
         grades = data_frame.select('CURSO', 'A', 'B', 'C', 'D', 'F', 'W', 'TOTAL').groupBy('CURSO').sum()
-        grades.withColumn("avg", (grades["sum(A)"]*4 + grades["sum(B)"]*3 + grades["sum(C)"]*2 + grades["sum(D)"]*1)/grades["sum(TOTAL)"])
-        data_frames.append(grades)
+        grades = grades.withColumn(f"avg_{count}", (grades["sum(A)"]*4 + grades["sum(B)"]*3 + grades["sum(C)"]*2 + grades["sum(D)"]*1)/grades["sum(TOTAL)"])
 
+        grades = grades.withColumnRenamed('sum(A)', f'A_{count}').withColumnRenamed('sum(B)', f'B_{count}').\
+            withColumnRenamed('sum(C)', f'C_{count}').withColumnRenamed('sum(D)', f'D_{count}').\
+            withColumnRenamed('sum(F)', f'F_{count}').withColumnRenamed('sum(TOTAL)', f'TOTAL_{count}').\
+            withColumnRenamed('sum(W)', f'W_{count}')
+
+        data_frames.append(grades)
+        count += 1
 joined = data_frames[0]
 count = 0
 for index in range(1, len(data_frames)):
     joined = joined.join(data_frames[index], on=["CURSO"], how='inner')
     count += 1
 
-joined.show()
+joined = joined.withColumn("avg_avg", sum(joined[col] for col in joined.columns if "avg" in col)/len(data_frames))
 
+joined.show()
